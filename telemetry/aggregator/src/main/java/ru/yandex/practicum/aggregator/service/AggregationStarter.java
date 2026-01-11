38e826a16a1f18169ff58e8bc43dc215faad3b44
Dudenko Vadim <dudenko.vadim@gmail.com>
2026-01-11 22:26:48 +0300

    3-aggregator

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/service/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/service/AggregationStarter.java
@@ -41,11 +41,9 @@ public class AggregationStarter {
         // Consumer
         Properties consumerProps = new Properties();
         consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-group");
         consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventAvroDeserializer.class);
-        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         consumer = new KafkaConsumer<>(consumerProps);
 
         // Producer
